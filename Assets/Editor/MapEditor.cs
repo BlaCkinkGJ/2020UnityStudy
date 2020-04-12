5913e64bea8aefedffed1e14b792c0adfbfc147c
BlaCkinkGJ <ss5kijun@gmail.com>
2020-04-12 18:11:45 +0900

    Editor

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor
+{
+    // 매 프레임마다 새로운 GUI를 그리도록 한다.
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MapGenerator map = target as MapGenerator;
+
+        map.GenerateMap();
+    }
+}
